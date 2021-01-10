0034aec730697058ee591fa5e3fa2dd7e23c3253
Flávio Carvalho <flaviolavespw@outlook.com>
2021-01-10 10:37:38 -0300

    Search order location

---
+++ front-web/src/Orders/types.ts
@@ -0,0 +1,15 @@
+
+
+export type Product = {
+    id: number;
+    name: string;
+    price: number;
+    description: String;
+    imageUri: string;
+}
+
+export type OrderLocationdata = {
+    latitude:  number;
+    longitude: number;
+    address: string;
+}
